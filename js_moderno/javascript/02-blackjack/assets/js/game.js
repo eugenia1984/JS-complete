44c91223ed1890e7c3f4c8d6fa5aad67871176fe
eugenia1984 <costamariaeugenia1@gmail.com>
2024-07-04 18:29:21 +0200

    feat: continuo con blackjack

--- js_moderno/javascript/02-blackjack/assets/js/game.js
+++ js_moderno/javascript/02-blackjack/assets/js/game.js
@@ -12,14 +12,14 @@ let pointsPlayer = 0;
 let pointsComputer = 0;
 
 // HTML References
-const btnAsk = document.querySelector('#btnAsk');
-const btnNew = document.querySelector('#btnNew');
-const btnStop = document.querySelector('#btnStop');
+const btnAsk = document.querySelector("#btnAsk");
+const btnNew = document.querySelector("#btnNew");
+const btnStop = document.querySelector("#btnStop");
 
-const pointsHTML = document.querySelectorAll('small');
+const pointsHTML = document.querySelectorAll("small");
 
-const divPlayerCards = document.querySelector('#player-cards');
-const divComputerCards = document.querySelector('#computer-cards');
+const divPlayerCards = document.querySelector("#player-cards");
+const divComputerCards = document.querySelector("#computer-cards");
 
 // Create a new Deck
 const createDeck = () => {
@@ -59,44 +59,77 @@ const cardValue = (card) => {
 
 createDeck();
 
-/***** Events ****/ 
 const computerTime = (minimumPoints) => {
   do {
-  const card = askForOneCard();
+    const card = askForOneCard();
 
-  pointsComputer = pointsComputer + cardValue(card);
-  pointsHTML[1].innerText = pointsComputer;
+    pointsComputer = pointsComputer + cardValue(card);
+    pointsHTML[1].innerText = pointsComputer;
 
-  const imgCard = document.createElement('img');
-  imgCard.src = `assets/cartas/${card}.png`;
-  imgCard.classList.add('game-card');
-  divComputerCards.append(imgCard );  
+    const imgCard = document.createElement("img");
+    imgCard.src = `assets/cartas/${card}.png`;
+    imgCard.classList.add("game-card");
+    divComputerCards.append(imgCard);
 
-  if(minimumPoints > 21) {
-    break;
-  }
+    if (minimumPoints > 21) {
+      break;
+    }
+  } while (pointsComputer < minimumPoints && minimumPoints <= 21);
 
-  } while( (pointsComputer < minimumPoints) && (minimumPoints <= 21) );
-}
+  setTimeout(() => {
+    if (pointsComputer === minimumPoints) {
+      alert("Nobody wins");
+    } else if (minimumPoints > 21) {
+      alert("Computer wins");
+    } else if (pointsComputer > 21) {
+      alert("Player wins");
+    } else {
+      alert("Computer wins");
+    }
+  }, 90);
+};
 
-btnAsk.addEventListener('click', () => {
+/***** Events ****/
+btnAsk.addEventListener("click", () => {
   const card = askForOneCard();
 
   pointsPlayer = pointsPlayer + cardValue(card);
   pointsHTML[0].innerText = pointsPlayer;
 
-  const imgCard = document.createElement('img');
+  const imgCard = document.createElement("img");
   imgCard.src = `assets/cartas/${card}.png`;
-  imgCard.classList.add('game-card');
-  divPlayerCards.append(imgCard );  
+  imgCard.classList.add("game-card");
+  divPlayerCards.append(imgCard);
 
-  if(pointsPlayer > 21) {
-    console.warn('You lost');
+  if (pointsPlayer > 21) {
     btnAsk.disabled = true;
+    btnStop.disabled = true;
     computerTime(pointsPlayer);
   } else if (pointsPlayer === 21) {
-    console.warn('*** 21 points ***')
+    btnAsk.disabled = true;
+    btnStop.disabled = true;
+    computerTime(pointsPlayer);
   }
+});
+
+btnStop.addEventListener("click", () => {
+  btnAsk.disabled = true;
+  btnStop.disabled = true;
+
+  computerTime(pointsPlayer);
+});
+
+btnNew.addEventListener('click', () => {
+  deck = createDeck();
+  pointsPlayer = 0;
+  pointsComputer = 0;
+
+  pointsHTML[0].innerText = 0;
+  pointsHTML[1].innerText = 0;
+
+  divPlayerCards.innerHTML = '';
+  divComputerCards.innerHTML = '';
+
+  btnAsk.disabled = false;
+  btnStop.disabled = false;
 })
-
-
