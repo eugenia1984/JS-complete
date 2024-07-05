9f302e8a7ddbfeac63e57358901c2871d5481539
eugenia1984 <costamariaeugenia1@gmail.com>
2024-07-05 15:04:08 +0200

    feat: agrego problema

--- js_moderno/javascript/01-fundamentos/assets/js/classes/problem.js
+++ js_moderno/javascript/01-fundamentos/assets/js/classes/problem.js
@@ -13,7 +13,30 @@ const pedro = {
 
 const melissa = {
   nombre: 'Melissa',
-  edad: 35
+  edad: 35,
+  imprimir() {
+    console.log(`Nombre : ${this.nombre} - edad: ${edad}`)
+  }
 }
 
 fer.imprimir();
+pedro.imprimir();
+
+// Era la alternativa, para crear objetos
+// Pero no puedo establecer propiedades privadas
+function Persona(nombre, edad = 1) {
+  this.nombre = nombre;
+  this.edad = edad;
+
+  this.imprimir = function() {
+    console.log(`Nombre : ${this.nombre} - edad: ${edad}`)
+  }
+}
+
+// Instancio con new
+const maria = new Persona('Maria', 18);
+const mariana = new Persona('Mariana', 28);
+console.log(maria);
+maria.imprimir();
+console.log(mariana);
+mariana.imprimir();
