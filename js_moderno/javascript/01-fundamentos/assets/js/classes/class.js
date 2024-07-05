cbc3b792c4f3379d4009e18a42584c1fe91995a2
eugenia1984 <costamariaeugenia1@gmail.com>
2024-07-05 16:54:29 +0200

    feat: agrego propiedades estaticas

--- js_moderno/javascript/01-fundamentos/assets/js/classes/class.js
+++ js_moderno/javascript/01-fundamentos/assets/js/classes/class.js
@@ -1,6 +1,15 @@
 console.warn('**** Classes ****');
 
 class Person {
+  // Atributo estatico
+  static _count = 0;
+  static get count() {
+    return Person._count;
+  }
+  static message() {
+    console.log('Hi, I am a static method')
+  }
+
   // Atributos
   name = '';
   code = '';
@@ -13,6 +22,8 @@ class Person {
     this.code = code;
     this.phrase = phrase;
     this.favoriteFood = favoriteFood;
+
+    Persona._count++;
   }
 
   // Setter -> establece un valor
@@ -43,3 +54,11 @@ spiderMan.myPhrase();
 console.log('*************');
 spiderMan.setFavoriteFood = 'Pizza';
 console.log(spiderMan.getFavoriteFood);
+console.log('Instance of Persona: ',Person.count);
+console.log('*************');
+Person.message();
+// Se pueden crear atributos static por fuera de la clase
+// Pero es mejor definirla DENTRO de la class
+// Primero van lso static y luego las propiedades
+Persona.atributeFromOutside = 'Static attribute generated outside the class';
+console.log(Person.atributeFromOutside)
