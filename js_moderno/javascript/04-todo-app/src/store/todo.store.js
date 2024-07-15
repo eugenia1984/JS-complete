01cd349f33180c7c8814b25e978625dd2e9eb215
eugenia1984 <costamariaeugenia1@gmail.com>
2024-07-15 10:21:51 +0200

    feat: creo las funciones del store

--- js_moderno/javascript/04-todo-app/src/store/todo.store.js
+++ js_moderno/javascript/04-todo-app/src/store/todo.store.js
@@ -21,6 +21,53 @@ const initStore = () => {
   console.log("state: ", state);
 };
 
+
+const loadStore = () => {
+  throw new Error('Not implemented');
+}
+
+/**
+ * 
+ * @param {String} description - todo description
+ */
+const addTodo = (description) => {
+  throw new Error('Not implemented');
+}
+
+/**
+ * 
+ * @param {String} todoId - todo unique identifier
+ */
+const toggleTodo = ( todoId) => {
+  throw new Error('Not implemented');
+} 
+
+/**
+ * 
+ * @param {String} todoId - todo unique identifier
+ */
+const deleteTodo = ( todoId) => {
+  throw new Error('Not implemented');
+}
+
+const deleteCompleted = () => {
+  throw new Error('Not implemented');
+}
+
+const setFilter = (newFilter = Filters.All) => {
+  throw new Error('Not implemented');
+}
+
+const getCurrentFilter = () => {
+  throw new Error('Not implemented');
+}
+
 export default {
   initStore,
+  loadStore,
+  toggleTodo,
+  deleteTodo,
+  deleteCompleted,
+  setFilter,
+  getCurrentFilter
 };
