8570d7f84bcf595e3d68fe27b485e0d89183e33f
eugenia1984 <costamariaeugenia1@gmail.com>
2024-07-16 14:43:40 +0200

    feat: agrego un todo nuevo

--- js_moderno/javascript/04-todo-app/src/todos/app.js
+++ js_moderno/javascript/04-todo-app/src/todos/app.js
@@ -3,8 +3,10 @@ import todoStore from '../store/todo.store';
 import { renderTodos } from './use-cases';
 
 const elementIDs = {
-  TodoList: '.todo-list'
+  TodoList: '.todo-list',
+  NewTodoInput: '#new-todo-input'
 }
+
 /**
  * 
  * @param {String} elementId 
@@ -15,10 +17,24 @@ export const App = (elementId) => {
     renderTodos(elementIDs.TodoList, todos );
   }
 
+  // When App is loaded
   (() => {
     const app = document.createElement('div');
     app.innerHTML = html;
     document.querySelector(elementId).append(app);
     displayTodos();
   })();
+
+  // HTML References
+  const newDescriptionInput = document.querySelector(elementIDs.NewTodoInput);
+
+  // Listeners
+  newDescriptionInput.addEventListener('keyup', (event) => {
+    if(event.keyCode !== 13) return;
+    if( event.target.value.trim().length === 0) return;
+
+    todoStore.addTodo(event.target.value);
+    displayTodos();
+    event.target.value = '';
+  })
 }
