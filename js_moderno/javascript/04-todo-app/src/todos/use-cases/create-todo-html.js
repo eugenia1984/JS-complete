324cc64373411b875d9e6117a49010a6ca95eef2
eugenia1984 <costamariaeugenia1@gmail.com>
2024-07-16 14:03:34 +0200

    feat: modifico el crear el todo html

--- js_moderno/javascript/04-todo-app/src/todos/use-cases/create-todo-html.js
+++ js_moderno/javascript/04-todo-app/src/todos/use-cases/create-todo-html.js
@@ -1,16 +1,27 @@
 import { Todo } from "../models/todo.model";
 
 /**
- * 
- * @param {Todo} todo 
+ *
+ * @param {Todo} todo
  */
 export const createTodoHtml = (todo) => {
-  if( !todo) throw new Error('A Todo object is required');
+  if (!todo) throw new Error("A Todo object is required");
 
-  const html = `<h>${todo.description}</h>`
-
-  const liElement = document.createElement('li');
+  const html = `
+    <div class="view">
+      <input class="toggle" type="checkbox" ${todo.done? 'checked': ''}>
+      <label>${todo.description}</label>
+      <button class="destroy"></button>
+    </div>
+    <input class="edit" value="Create a TodoMVC template">
+  `;
+  const liElement = document.createElement("li");
   liElement.innerHTML = html;
+  liElement.setAttribute("data-id", todo.id);
+  
+  if(todo.done) {
+    liElement.classList.add('completed');
+  }
 
   return liElement;
-}
+};
