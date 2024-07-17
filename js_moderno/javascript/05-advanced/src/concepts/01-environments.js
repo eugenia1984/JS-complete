8342679c83eb1ac9172c058efc7f4cdb73a4fe8b
eugenia1984 <costamariaeugenia1@gmail.com>
2024-07-17 18:48:22 +0200

    feat: muestro las key

--- js_moderno/javascript/05-advanced/src/concepts/01-environments.js
+++ js_moderno/javascript/05-advanced/src/concepts/01-environments.js
@@ -4,9 +4,15 @@
  */
 export const environmentsComponent = ( element) => {
   console.log(import.meta.env);
-  
+
   const html = `
-    variables:
+    Dev: ${import.meta.env.DEV} <br />
+    Prod: ${import.meta.env.PROD} <br />
+    SSR: ${import.meta.env.SSR} <br />
+    <hr />
+    Variables: <br/>
+    KEY: ${import.meta.env.VITE_API_KEY} <br />
+    URL: ${import.meta.env.VITE_BASE_URL} <br />
   `;
   element.innerHTML = html;
 }
