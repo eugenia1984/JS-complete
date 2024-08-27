9aa7508086974f7995f51ab7e9c01bda8827a5fd
eugenia1984 <costamariaeugenia1@gmail.com>
2024-08-27 19:16:35 +0200

    feat: agrego el modelo del usuario

--- js_moderno/javascript/07-crud/src/users/store/users-store.js
+++ js_moderno/javascript/07-crud/src/users/store/users-store.js
@@ -1,4 +1,5 @@
-import { loadUsersByPage } from '../use-cases/load-users-by-page'
+import { loadUsersByPage } from '../use-cases/load-users-by-page';
+
 const state = {
   currentPage: 0,
   users: [],
