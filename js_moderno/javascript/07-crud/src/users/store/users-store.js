0cf50a109b88b011b9d3720e9bc380e320c81a40
eugenia1984 <costamariaeugenia1@gmail.com>
2024-08-27 18:53:09 +0200

    feat: agrego el store

--- js_moderno/javascript/07-crud/src/users/store/users-store.js
+++ js_moderno/javascript/07-crud/src/users/store/users-store.js
@@ -2,3 +2,28 @@ const state = {
   currentPage: 0,
   users: [],
 };
+
+const loadNextPage = () => {
+  throw new Error('Not implemented')
+}
+
+const loadPreviousPage = () => {
+  throw new Error('Not implemented')
+}
+
+const onUserChanged = () => {
+  throw new Error('Not implemented')
+}
+
+const reloadPage = () => {
+  throw new Error('Not implemented')
+}
+
+export default {
+  loadNextPage,
+  loadPreviousPage,
+  onUserChanged,
+  reloadPage,
+  getUsers: () => [...state.users],
+  getCurrentPage: () => state.currentPage
+}
