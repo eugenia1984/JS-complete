8774d4ff667883d23ab1159b52ef375878aadb25
eugenia1984 <costamariaeugenia1@gmail.com>
2024-08-27 13:20:56 +0200

    feat : add http request

--- js_moderno/javascript/06-http-app/src/breaking-bad/breaking-bad-app.js
+++ js_moderno/javascript/06-http-app/src/breaking-bad/breaking-bad-app.js
@@ -2,8 +2,11 @@
  * @returns { Promise<Object> } // quote information
  */
 const fetchQuote = async () => {
+  // const res = await fetch('https://www.breakingbadapi.com/api/quote/random');
   const res = await fetch("https://breakingbadapi.com/api/quote/random" , { mode: 'no-cors' });
-  const data = res.json();
+  const data = await res.json();
+
+  console.log(data);
 
   return data[0];
 };
@@ -14,10 +17,30 @@ const fetchQuote = async () => {
  */
 export const BreakingBadApp = async (element) => {
   document.querySelector("#app-title").innerHTML = "BreakingBad App";
-
   element.innerHTML = "Loading . . .";
+  // await fetchQuote();
+  
+  // HTML element reference
+  const quoteLabel = document.createElement('blockquote');
+  const authorLabel = document.createElement('h3');
+  const nextQuoteButton = document.createElement('button');
+  nextQuoteButton.innerText = 'Next quote';
 
-  const quote = await fetchQuote();
+  const renderQuote = ( data ) => {
+    quoteLabel.innerHTML = data.quote;
+    authLabel.innerHTML = data.author;
+    element.replaceChildren(quoteLabel, authorLabel, nextQuoteButton);
+  }
 
-  element.innerHTML = 'Data searched'
+    // Add Listener
+    nextQuoteButton.addEventListener('click', async() => {
+      element.innerHTML = 'Loading...';
+      const quote = await fetchQuote();
+      renderQuote( quote );
+  })
+
+  // fetchQuote()
+  //   .then( data => renderQuote(data))
+  fetchQuote()
+    .then( renderQuote );
 };
