2d615999f98d31a3a18a65a03c45ba47d3b7db69
eugenia1984 <costamariaeugenia1@gmail.com>
2024-08-29 15:06:46 +0200

    feat: agrego el getUserById para actualizar el form si seleccino un usuario

--- js_moderno/javascript/07-crud/src/users/presentation/render-table/render-table.js
+++ js_moderno/javascript/07-crud/src/users/presentation/render-table/render-table.js
@@ -1,4 +1,5 @@
 import usersStore from "../../store/users-store";
+import { showModal } from "../render-modal/render-modal";
 import "./render-table.css";
 
 let table;
@@ -24,6 +25,19 @@ const createTable = () => {
 }
 
 /**
+ * 
+ * @param {MouseEvent} event 
+ */
+const tableSelectListener = ( event ) => {
+  const element = event.target.closest('.select-user');
+  
+  if( !element ) return;
+
+  const id = element.getAttribute('data-id');
+  showModal(id);
+}
+
+/**
  *
  * @param {HTMLDivElement} element
  */
@@ -34,7 +48,9 @@ export const renderTable = (element) => {
     table = createTable();
     element.append( table );
 
-    // TODO: listeners
+    // EventListeners
+    // table.addEventListener('click', (event) => tableSelectListener(event))
+    table.addEventListener('click', tableSelectListener)
   }
 
   let tableHTML = '';
@@ -47,9 +63,9 @@ export const renderTable = (element) => {
         <td>${user.lastName}</td>
         <td>${user.isActive}</td>
         <td>
-          <a href="#/" data-id="${user.id}">Select</a>
+          <a href="#/" class="select-user" data-id="${user.id}">Select</a>
           |
-          <a href="#/" data-id="${user.id}">Delete</a>
+          <a href="#/" class="delete-user" data-id="${user.id}">Delete</a>
         </td>
       </tr>
     `
